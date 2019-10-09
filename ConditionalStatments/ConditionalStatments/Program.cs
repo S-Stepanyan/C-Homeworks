ff6b0db77ea127e4e54b3b43a1db84fc784f1c74
User <User@USER-PC>
2019-10-09 21:55:26 +0400

    If-Else Staetments

---
+++ ConditionalStatments/ConditionalStatments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConditionalStatments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                if (num1 % 2 == 0)
+                    Console.WriteLine("num1>num2 zuyg");
+                else
+                    Console.WriteLine("num1<num2 kent"); 
+            }
+            else if (num1 < num2)
+            {
+                if (num2 % 2 == 0)
+                {
+                    Console.WriteLine("num2>num1 zuyg");
+
+                }
+                else { Console.WriteLine("num2>num1 kent"); }
+            }
+            else
+            {
+                if (num2 % 2 == 0)
+                    Console.WriteLine("num1=num2 zuyg");
+                else
+                    Console.WriteLine("num1=num2 kent");
+            }
+        }
+    }
+}
